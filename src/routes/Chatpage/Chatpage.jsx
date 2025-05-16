a97969abe25dd19ea3a72129f53bbce7224f02b3
LVThuc <24521748gm.uit.edu.vn>
2025-05-17 05:17:06 +0700

    daily

--- src/routes/Chatpage/Chatpage.jsx
+++ src/routes/Chatpage/Chatpage.jsx
@@ -1,8 +1,41 @@
+import NewPrompt from '../../components/NewPrompt/NewPrompt'
 import './Chatpage.css'
 
+
 const Chatpage = () => {
+
   return (
-    <div className='Chatpage'>Chatpage</div>
+    <div className='Chatpage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam praesentium aliquam dolor officia placeat omnis aperiam, vero inventore, itaque minus cumque ab obcaecati voluptatem. Numquam voluptatum quidem deleniti doloremque repellat.</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <div className ='message'>Test Message</div>
+          <div className ='message user'>Test Message from user</div>
+          <NewPrompt/> 
+        </div>
+      </div>
+    </div>
   )
 }
 
